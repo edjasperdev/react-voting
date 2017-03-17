74e3924993dddbbe4f2155f57640924ed83e1b64
Edward Leon Jasper <edwardjasperjr@gmail.com>
2017-03-17 17:51:18 -0400

    add reducer

--- src/core.js
+++ src/core.js
@@ -1,38 +1,40 @@
 import { List, Map } from 'immutable'
 
+export const INITIAL_STATE = Map();
+
 export function setEntries(state, entries) {
 	return sta te.set('entries', List(entries));
 }
 
-	export function vote(state, entry){
-		return state.updateIn(
-			['vote', 'tally', entry],
-			0,
-			tally => tally + 1
-		);
-	}
+export function vote(state, entry){
+	return state.updateIn(
+		['vote', 'tally', entry],
+		0,
+		tally => tally + 1
+	);
+}
 
-	function getWinners(vote){
-		if (!vote) return [];
-		const [a,b] = vote.get('pair');
-		const aVotes = vote.getIn(['tally', a], 0); 
-		const bVotes = vote.getIn(['tally', b], 0);
-		if 		 	(aVotes > bVotes) return [a];  
-		else if (aVotes < bVotes) return [b];  
-		else 											return [a, b];  
-	}
+function getWinners(vote){
+	if (!vote) return [];
+	const [a,b] = vote.get('pair');
+	const aVotes = vote.getIn(['tally', a], 0); 
+	const bVotes = vote.getIn(['tally', b], 0);
+	if 		 	(aVotes > bVotes) return [a];  
+	else if (aVotes < bVotes) return [b];  
+	else 											return [a, b];  
+}
 
-	export function next(state){
-		const entries = state.get('entries')
-													.concat(getWinners(state.get('vote')));
-		if (entries.size === 1){
-			return state.remove('vote')
-									.remove('entries')
-									.set('winner', entries.first());							
-		} else {
+export function next(state){
+	const entries = state.get('entries')
+												.concat(getWinners(state.get('vote')));
+	if (entries.size === 1){
+		return state.remove('vote')
+								.remove('entries')
+								.set('winner', entries.first());							
+	} else {
 		return state.merge({
 			vote: Map({pair: entried.take(2)}),
 			entries: entries.skip(2)
 		});
-	}
+	}	
 }
